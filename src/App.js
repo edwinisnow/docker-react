05a8c631d27b8a6c53df851e93727692c51dc570
edwinisnow <edwin04@gmail.com>
2022-08-04 17:13:56 +0100

    changed text reverted

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed
+          Learn React
         </a>
       </header>
     </div>
